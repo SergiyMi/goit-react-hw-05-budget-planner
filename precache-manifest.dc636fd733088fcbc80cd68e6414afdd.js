a58726f6db48257ac5f4a68e3a78e0a3d7bb13a3
SergiyMi <bladeneo@i.ua>
2019-12-27 15:24:00 +0200

    Updates

---
+++ precache-manifest.dc636fd733088fcbc80cd68e6414afdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35c0d799c5c161044d434ab3b7905cd9",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "88961fdd38ea51c8bdcf",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.4c524b13.chunk.css"
+  },
+  {
+    "revision": "0ea29e66ec964846c8a3",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.d72ca5af.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.d72ca5af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "88961fdd38ea51c8bdcf",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.48287103.chunk.js"
+  },
+  {
+    "revision": "5898b43781fcd94828d3",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime-main.8d80df19.js"
+  }
+]);
